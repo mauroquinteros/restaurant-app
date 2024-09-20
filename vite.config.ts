cb87915056f2b90f49f47a8687ab2d4e84185a62
Mauro Quinteros <quinterosmauro0599@gmail.com>
2024-09-19 20:54:47 -0500

    chore: remove alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@common": path.resolve(__dirname, "./src/common/index.ts"),
-      "@icons": path.resolve(__dirname, "./src/icons/index.ts"),
-      "@components": path.resolve(__dirname, "./src/components/index.ts"),
-      "@pages": path.resolve(__dirname, "./src/pages/"),
-      "@hooks": path.resolve(__dirname, "./src/hooks/index.ts"),
-    },
-  },
 });
